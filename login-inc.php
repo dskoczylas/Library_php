2c41ab453dbc2d619b6c796ed3e6adcba885f25a
dskoczylas <92728373+dskoczylas@users.noreply.github.com>
2022-10-13 15:06:30 +0200

    Add files via upload

---
+++ login-inc.php
@@ -0,0 +1,46 @@
+<?php
+
+    require_once 'database.php';
+
+    $username = $_POST['user'];
+    $password = $_POST['password'];
+
+    if(empty($username) || empty($password)){
+        header("Location: ../login.php?error=enternameorpassword");
+        exit();
+    }else{
+          $sql = "SELECT * FROM users WHERE username = ?;";
+          $stmt = mysqli_stmt_init($conn);
+          mysqli_stmt_prepare($stmt,$sql);
+          mysqli_stmt_bind_param($stmt,"s",$username);
+          mysqli_stmt_execute($stmt);
+          $result = mysqli_stmt_get_result($stmt);
+            if($row = mysqli_fetch_assoc($result)){
+                $passCheck = password_verify($password,$row['password']);
+
+                if ($passCheck === true){
+                  header("Location: ../login.php?error=goodpassword");
+                  exit();
+                }else{
+                  header("Location: ../login.php?error=wrongpassword");
+                  exit();
+                }
+
+
+            }else{
+              header("Location: ../login.php?error=nouserexist");
+              exit();
+            }
+
+
+
+    }
+
+
+
+
+
+
+
+
+ ?>
